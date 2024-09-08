5cbf4996d095f93036a8eea398d0b6d5fe4845a6
lancer1999fd <lancer1999fd@proton.me>
2024-09-09 01:02:37 +0200

    Add social networking system

--- system/urls.py
+++ system/urls.py
@@ -1,22 +1,68 @@
 from django.urls import path
 
 from system.views import (
+    AcceptFriendRequestView,
     AccountView,
+    BlockUserView,
+    CancelFriendRequestView,
+    DeclineFriendRequestView,
+    DetailUserFriendsView,
+    DetailUserView,
     DisclaimerView,
     HomeView,
     ImpressumView,
+    NotificationsView,
     PrivacyView,
+    RemoveFriendView,
+    SearchView,
+    SendFriendRequestView,
     SettingsDeleteView,
     SettingsUpdateView,
     SettingsView,
     SignUpView,
     TermsView,
+    UnblockUserView,
 )
 
 from . import views
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
+    path("notifications/", NotificationsView.as_view(), name="notifications"),
+    path(
+        "notifications/accept/<int:pk>/",
+        AcceptFriendRequestView.as_view(),
+        name="accept_friend_request",
+    ),
+    path(
+        "notifications/decline/<int:pk>/",
+        DeclineFriendRequestView.as_view(),
+        name="decline_friend_request",
+    ),
+    path("search/", SearchView.as_view(), name="search"),
+    path("profile/<int:pk>", DetailUserView.as_view(), name="profile_detail"),
+    path(
+        "profile/<int:pk>/friends",
+        DetailUserFriendsView.as_view(),
+        name="profile_friends",
+    ),
+    path(
+        "profile/<int:pk>/send_request/",
+        SendFriendRequestView.as_view(),
+        name="send_friend_request",
+    ),
+    path(
+        "profile/<int:pk>/cancel_request/",
+        CancelFriendRequestView.as_view(),
+        name="cancel_friend_request",
+    ),
+    path(
+        "profile/<int:pk>/remove_friend/",
+        RemoveFriendView.as_view(),
+        name="remove_friend",
+    ),
+    path("profile/<int:pk>/block/", BlockUserView.as_view(), name="block_user"),
+    path("profile/<int:pk>/unblock/", UnblockUserView.as_view(), name="unblock_user"),
     path("account/", AccountView.as_view(), name="account"),
     path("settings/", SettingsView.as_view(), name="settings"),
     path(
